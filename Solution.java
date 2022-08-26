118e8d941a351746911868d1e40d4d3f42c80a7e
DaAnunnaki <93460161+DaAnunnaki@users.noreply.github.com>
2022-08-26 01:24:26 -0500

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -2,53 +2,59 @@ import java.util.*;
 
     public class Solution {
 
-
-        public static int age;
+        public static int[] age;
         public static String[] names;
         public static int num;
+        public static String[] yn;
+        public static String[] first;
+
         public static void main(String[] args) {
 
             Scanner scan = new Scanner(System.in);
 
             num = scan.nextInt();
-            String[] names;
+
+            first = new String[num +1];
+            names = new String[num +1];
+            age = new int[num +1];
+            yn = new String[num +1];
 
             for(int i =0; i<num; i++){
-                String firstName = scan.nextLine();
-                String lastName = scan.nextLine();
+                String firstName = scan.next();
+                String lastName = scan.next();
 
-                String date = scan.nextLine();
+                String month = scan.next();
                 int day = scan.nextInt();
-                String dot = scan.nextLine();
 
                 int year = scan.nextInt();
-                String yesNO = scan.nextLine();
+                String yesNo = scan.next();
                 
-                System.out.print(firstName);
-
-                Solution.sortName(lastName);            
-
-                System.out.print()
+                int agee = 2015-year;
+                if(!(month.contains("Jan")||!month.contains("Feb")||month.contains("Mar")||month.contains("Apr"))){
+                    agee--;
+                }
+                
+                first[i] = firstName;
+                names[i]=lastName;
+                if(yesNo.contains("yes")) {
+                    age[i]= agee; 
+                    yn [i] = "Included";   
+                }
+                else if(yesNo.contains("no")) {
+                    age[i]= 00; 
+                    yn [i] = "excluded";   
+                }    
             }
-            
-            
             scan.close();
-            
-            System.out.println(names);
-
-
-
-
+            System.out.println(Solution.sort());
         }
 
-        public static void sortName(String name) {
-            for(int i =0; i<num; i++){
-                names[i]=name;
-            }
-        }
-
-        public static print() {
+        public static String sort() {
             String temp;
+            int newAge;
+            String str;
+            String ff;
+            String output = "";
             for (int i = 0; i < num; i++) {
                 for (int j = i + 1; j < num; j++) {
                     if (names[i].compareTo(names[j]) > 0) {// to compare one string with other strings
@@ -56,14 +62,25 @@ import java.util.*;
                         temp = names[i];
                         names[i] = names[j];
                         names[j] = temp;
+
+                        newAge = age[i];
+                        age[i] = age[j];
+                        age[j] = newAge;
+
+                        str = yn[i];
+                        yn[i] = yn[j];
+                        yn[j] = str;
+
+                        ff = first[i];
+                        first[i] = first[j];
+                        first[j] = ff;
+
                     }
                 }
             }
-
-
-            return ;
+            for(int o = 0; o<num; o++) {
+                output += first[o]+ " "+ names[o] + " " + age[o] + " " +yn[o] + "\n";
+            }
+            return output;
         }
     }
-
-
-
